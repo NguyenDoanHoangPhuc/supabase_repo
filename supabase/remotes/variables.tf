e8bf6b8eb24fc54a2d662423d57141ecb582d999
HoangPhuc-edward <hoangphuc090104@gmail.com>
2024-11-18 22:47:47 +0700

    delete some tf

--- supabase/remotes/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "supabase_access_token" {
-  description = "Access token for Supabase"
-  type        = string
-}
-
-variable "supabase_project_id" {
-  description = "Project ID for Supabase"
-  type        = string
-}
